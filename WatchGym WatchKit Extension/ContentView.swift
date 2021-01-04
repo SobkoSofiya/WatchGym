37777f4be82e70aa63aec10ac630e3d2565b883e
SobkoSofiya <sobkosofiya23@gmail.com>
2021-01-04 13:45:17 +0300

    Initial Commit

---
+++ WatchGym WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchGym WatchKit Extension
+//
+//  Created by Sofi on 04.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchGym WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  WatchGym WatchKit Extension
+//
+//  Created by Sofi on 04.01.2021.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
